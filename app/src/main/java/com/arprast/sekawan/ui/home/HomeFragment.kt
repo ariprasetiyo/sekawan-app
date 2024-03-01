1e90dd88d88e95d10a010f378ed79d5fb18de781
ariprasetiyo <prasetiyooo@gmail.com>
2024-03-01 21:54:02 +0700

    revamp input customer data

--- app/src/main/java/com/arprast/sekawan/ui/home/HomeFragment.kt
+++ app/src/main/java/com/arprast/sekawan/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.ContentValues
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -19,6 +20,7 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
@@ -34,6 +36,8 @@ import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textview.MaterialTextView
+import com.rengwuxian.materialedittext.MaterialEditText
 import java.io.File
 
 class HomeFragment : Fragment() {
@@ -110,11 +114,17 @@ class HomeFragment : Fragment() {
 
         registerId = RegistrationActivityType.OPEN_CAM_FOR_PHOTO
         val values = ContentValues()
-        values.put(MediaStore.Images.Media.DISPLAY_NAME, "capture_image_${System.currentTimeMillis()}");
+        values.put(
+            MediaStore.Images.Media.DISPLAY_NAME,
+            "capture_image_${System.currentTimeMillis()}"
+        );
         values.put(MediaStore.Images.Media.DESCRIPTION, "this is description");
         values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
 
-        imageUri = fragmentActivity.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)!!
+        imageUri = fragmentActivity.contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            values
+        )!!
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
 //        activity?.setResult(Activity.RESULT_OK)
@@ -147,49 +157,104 @@ class HomeFragment : Fragment() {
 
     private fun showDialogTextInputLayout(context: Context) {
 
-        val layout = LinearLayout(context)
-        layout.orientation = LinearLayout.VERTICAL
-        layout.setPadding(
-            resources.getDimensionPixelOffset(R.dimen.dp_19),
-            0,
-            resources.getDimensionPixelOffset(R.dimen.dp_19),
-            0
-        )
-
-        layout.addView(inputCustomerName())
-        layout.addView(inputCustomerPhoneNumber())
-        layout.addView(inputCustomerEstimationDate())
-        layout.addView(inputCustomerAddress())
-        layout.addView(inputCustomerNote())
-
         val dateNow = Utils.getDateNow()
-        val alert = AlertDialog.Builder(context)
-            .setTitle("${getString(R.string.input_customer_title)} $dateNow")
-            .setMessage("Trx No ${Utils.getTrxNo()}")
-            .setView(layout)
-            .setPositiveButton(getString(R.string.button_save)) { dialog, _ ->
-                dialog.cancel()
-            }
-            .setNeutralButton(getString(R.string.button_cancel)) { dialog, _ ->
-                deleteFileImageCam()
-                dialog.cancel()
-            }.create()
+        val builder = AlertDialog.Builder(context)
+        // Get the layout inflater.
+        val inflater = requireActivity().layoutInflater
 
-        alert.show()
+        val viewAlertDialog = inflater.inflate(R.layout.dialog_after_open_cam, null)
+        val customerTrxDate = viewAlertDialog.findViewById<MaterialTextView>(R.id.input_customer_view_trx_date)
+        customerTrxDate.text = "$dateNow"
+        val customerTrxNo = viewAlertDialog.findViewById<MaterialTextView>(R.id.input_customer_view_trx_no)
+        customerTrxNo.text = "${getString(R.string.input_customer_trx_no)} ${Utils.getTrxNo()}"
+        val customerName = viewAlertDialog.findViewById<MaterialEditText>(R.id.input_customer_name)
+
+        val customerPhoneNo =
+            viewAlertDialog.findViewById<MaterialEditText>(R.id.input_customer_phone_number)
+        customerPhoneNo.inputType = (InputType.TYPE_CLASS_NUMBER)
+        customerPhoneNo.onFocusChangeListener = (View.OnFocusChangeListener { _, hasFocus ->
+            if (!hasFocus) {
+                val isValidPhoneNo = Utils.isValidPhoneNumber(customerPhoneNo.text.toString())
+                if (!isValidPhoneNo) {
+                    customerPhoneNo.hint = getString(R.string.input_customer_phone_number_warning)
+                    customerPhoneNo.setHintTextColor(resources.getColor(R.color.redColor))
+                    customerPhoneNo.text!!.clear()
+                }
+            }
+        })
+
+        val customerEstimationDate =
+            viewAlertDialog.findViewById<MaterialEditText>(R.id.input_customer_estimation_date);
+        customerEstimationDate.onFocusChangeListener = (View.OnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                val dialogFragment = Utils.DatePickerFragment(customerEstimationDate)
+                dialogFragment.show(fragmentManager, "datePicker")
+            }
+        })
+        val customerAddress =
+            viewAlertDialog.findViewById<MaterialEditText>(R.id.input_customer_address);
+        val customerNote = viewAlertDialog.findViewById<MaterialEditText>(R.id.input_customer_note);
+
+        val builderAlertDialog = builder.setView(viewAlertDialog)
+            // Add action buttons.
+            .setPositiveButton(getString(R.string.button_save),
+                DialogInterface.OnClickListener { dialog, id ->
+                    tostText("save success ${customerName.text}")
+                })
+            .setNeutralButton(getString(R.string.button_cancel),
+                DialogInterface.OnClickListener { dialog, id ->
+                    deleteFileImageCam()
+                    dialog.cancel()
+                })
+        builderAlertDialog.create()
+        builderAlertDialog.show()
+
+
+//        val layout = LinearLayout(context)
+//        layout.orientation = LinearLayout.VERTICAL
+//        layout.setPadding(
+//            resources.getDimensionPixelOffset(R.dimen.dp_19),
+//            0,
+//            resources.getDimensionPixelOffset(R.dimen.dp_19),
+//            0
+//        )
+//
+//        layout.addView(inputCustomerName())
+//        layout.addView(inputCustomerPhoneNumber())
+//        layout.addView(inputCustomerEstimationDate())
+//        layout.addView(inputCustomerAddress())
+//        layout.addView(inputCustomerNote())
+//
+//        val dateNow = Utils.getDateNow()
+//        val alert = AlertDialog.Builder(context)
+//            .setTitle("${getString(R.string.input_customer_title)} $dateNow")
+//            .setMessage("Trx No ${Utils.getTrxNo()}")
+//            .setView(layout)
+//            .setPositiveButton(getString(R.string.button_save)) { dialog, _ ->
+//                dialog.cancel()
+//            }
+//            .setNeutralButton(getString(R.string.button_cancel)) { dialog, _ ->
+//                deleteFileImageCam()
+//                dialog.cancel()
+//            }.create()
+//
+//        alert.show()
+//
+
     }
 
-    private fun inputCustomerName(): EditText {
-        val customerName = EditText(context)
+    private fun inputCustomerName(): MaterialEditText {
+        val customerName = MaterialEditText(context)
         customerName.hint = getString(R.string.input_customer_name)
         customerName.filters = (arrayOf<InputFilter>(InputFilter.LengthFilter(50)))
         customerName.isSingleLine = true
         customerName.onFocusChangeListener = (object : View.OnFocusChangeListener {
             override fun onFocusChange(view: View?, hasFocus: Boolean) {
                 if (!hasFocus) {
-                    if (customerName.text.isBlank()) {
+                    if (customerName.text!!.isBlank()) {
                         customerName.hint = getString(R.string.input_customer_name_warning)
                         customerName.setHintTextColor(getResources().getColor(R.color.redColor))
-                        customerName.text.clear()
+                        customerName.text!!.clear()
                     }
                 }
             }
@@ -197,8 +262,8 @@ class HomeFragment : Fragment() {
         return customerName
     }
 
-    private fun inputCustomerPhoneNumber(): EditText {
-        val customerPhoneNumber = EditText(context)
+    private fun inputCustomerPhoneNumber(): MaterialEditText {
+        val customerPhoneNumber = MaterialEditText(context)
         customerPhoneNumber.hint = getString(R.string.input_customer_phone_number)
         customerPhoneNumber.inputType = (InputType.TYPE_CLASS_NUMBER)
         customerPhoneNumber.filters = (arrayOf<InputFilter>(InputFilter.LengthFilter(14)))
@@ -211,7 +276,7 @@ class HomeFragment : Fragment() {
                         customerPhoneNumber.hint =
                             getString(R.string.input_customer_phone_number_warning)
                         customerPhoneNumber.setHintTextColor(getResources().getColor(R.color.redColor))
-                        customerPhoneNumber.text.clear()
+                        customerPhoneNumber.text!!.clear()
                     }
                 }
             }
@@ -219,8 +284,8 @@ class HomeFragment : Fragment() {
         return customerPhoneNumber
     }
 
-    private fun inputCustomerEstimationDate(): EditText {
-        val estimationDate = EditText(context)
+    private fun inputCustomerEstimationDate(): MaterialEditText {
+        val estimationDate = MaterialEditText(context)
         estimationDate.hint = getString(R.string.input_customer_estimation_date)
         estimationDate.onFocusChangeListener = (object : View.OnFocusChangeListener {
             override fun onFocusChange(p0: View?, hasFocus: Boolean) {
@@ -233,8 +298,8 @@ class HomeFragment : Fragment() {
         return estimationDate
     }
 
-    private fun inputCustomerAddress(): EditText {
-        val address = EditText(context)
+    private fun inputCustomerAddress(): MaterialEditText {
+        val address = MaterialEditText(context)
         address.hint = getString(R.string.input_customer_address)
         address.isSingleLine = false
         address.imeOptions = EditorInfo.IME_FLAG_NO_ENTER_ACTION
@@ -243,8 +308,8 @@ class HomeFragment : Fragment() {
         return address
     }
 
-    private fun inputCustomerNote(): EditText {
-        val note = EditText(context)
+    private fun inputCustomerNote(): MaterialEditText {
+        val note = MaterialEditText(context)
         note.hint = getString(R.string.input_customer_note)
         note.isSingleLine = false
         note.imeOptions = EditorInfo.IME_FLAG_NO_ENTER_ACTION
@@ -261,6 +326,15 @@ class HomeFragment : Fragment() {
             Utils.showTost(context, "file deleted :")
         } else {
             Utils.showTost(context, "file not deleted : ")
+        }
+    }
+
+    private fun tostText(text: String) {
+        activity.let {
+            val invalidateInputMesssage =
+                Toast.makeText(it, text, Toast.LENGTH_LONG)
+            invalidateInputMesssage.setGravity(Gravity.CENTER, 0, 0)
+            invalidateInputMesssage.show()
         }
     }
 }
